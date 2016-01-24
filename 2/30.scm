c691ad400e9dbbaef1c4dbdcd5dbadbe395b04a6
K.Kolotyuk <kk@7bits.com>
2016-01-25 00:03:06 +0600

    New exercises

---
+++ 2/30.scm
@@ -0,0 +1,12 @@
+(define (square-tree tree)
+  (cond ((null? tree) nil)
+        ((not (pair? tree)) (* tree tree))
+        (else (cons (scale-tree (car tree))
+                    (scale-tree (cdr tree))))))
+
+(define (square-tree tree)
+  (map (lambda (subtree)
+         (if (pair? subtree)
+           (square-tree subtree)
+           (* subtree subtree)))
+       tree))
